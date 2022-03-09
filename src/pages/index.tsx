a8cc9b9b93f75c347c75f6ce482fce57c5413d29
dhruvkelawala <dhruvrk2000@gmail.com>
2022-03-09 15:59:21 +0530

    use placeholder instead of defaultValue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,26 +3,40 @@ import {
   useStarknetCall,
   useStarknetInvoke,
   useStarknetTransactionManager,
-  Transaction
+  Transaction,
 } from '@starknet-react/core'
 import type { NextPage } from 'next'
 import { ConnectWallet } from '~/components/ConnectWallet'
 import { BigNumber } from 'bignumber.js'
 import { CallContractStringifyReturn, htmlParse } from '~/components/Contracts'
 import { useGameContract } from '~/hooks/game'
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form'
 
-const PRIME_BN = new BigNumber ('3618502788666131213697322783095070105623107215331596699973092056135872020481')
-const FP_BN = new BigNumber (10**12)
+const PRIME_BN = new BigNumber('3618502788666131213697322783095070105623107215331596699973092056135872020481')
+const FP_BN = new BigNumber(10 ** 12)
 
 const Home: NextPage = () => {
-
   const { account } = useStarknet()
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
   const { contract: gameContract } = useGameContract()
-  const { data: solutionCountValue } = useStarknetCall({ contract: gameContract, method: 'view_solution_found_count', args: {} })
-  const html_string = CallContractStringifyReturn (gameContract, "view_solution_records_as_html");
-  const { data, loading, error, reset, invoke:invokeSubmitMoveForLevel } = useStarknetInvoke({
+  const { data: solutionCountValue } = useStarknetCall({
+    contract: gameContract,
+    method: 'view_solution_found_count',
+    args: {},
+  })
+  const html_string = CallContractStringifyReturn(gameContract, 'view_solution_records_as_html')
+  const {
+    data,
+    loading,
+    error,
+    reset,
+    invoke: invokeSubmitMoveForLevel,
+  } = useStarknetInvoke({
     contract: gameContract,
     method: 'submit_move_for_level',
   })
@@ -41,30 +55,30 @@ const Home: NextPage = () => {
   // }
 
   const onSubmitMove = (data: any) => {
-      if (!account) {
-        console.log('user wallet not connected yet.')
-      }
-      else if (!gameContract) {
-        console.log('frontend not connected to game contract')
-      }
-      else {
-        BigNumber.config({ EXPONENTIAL_AT: 76 })
-        let data_movex_fp_bn = new BigNumber (data['moveXRequired']).multipliedBy(FP_BN)
-        let data_movey_fp_bn = new BigNumber (data['moveYRequired']).multipliedBy(FP_BN)
+    if (!account) {
+      console.log('user wallet not connected yet.')
+    } else if (!gameContract) {
+      console.log('frontend not connected to game contract')
+    } else {
+      BigNumber.config({ EXPONENTIAL_AT: 76 })
+      let data_movex_fp_bn = new BigNumber(data['moveXRequired']).multipliedBy(FP_BN)
+      let data_movey_fp_bn = new BigNumber(data['moveYRequired']).multipliedBy(FP_BN)
 
-        let move_x = data_movex_fp_bn.isPositive() ? data_movex_fp_bn : PRIME_BN.plus (data_movex_fp_bn)
-        let move_y = data_movey_fp_bn.isPositive() ? data_movey_fp_bn : PRIME_BN.plus (data_movey_fp_bn)
-        let move_x_str = move_x.toString()
-        let move_y_str = move_y.toString()
+      let move_x = data_movex_fp_bn.isPositive() ? data_movex_fp_bn : PRIME_BN.plus(data_movex_fp_bn)
+      let move_y = data_movey_fp_bn.isPositive() ? data_movey_fp_bn : PRIME_BN.plus(data_movey_fp_bn)
+      let move_x_str = move_x.toString()
+      let move_y_str = move_y.toString()
 
-        invokeSubmitMoveForLevel ({ args: {
+      invokeSubmitMoveForLevel({
+        args: {
           level: data['levelRequired'],
           move_x: move_x_str,
-          move_y: move_y_str
-        } })
-        console.log('submit move: ', data['levelRequired'], move_x_str, move_y_str)
-      }
+          move_y: move_y_str,
+        },
+      })
+      console.log('submit move: ', data['levelRequired'], move_x_str, move_y_str)
     }
+  }
 
   return (
     <div>
@@ -73,7 +87,14 @@ const Home: NextPage = () => {
 
       <h3>0xstrat v1.0</h3>
       <p>:: a Class B solve2mint system ::</p>
-      <p>   powered by <a href="https://github.com/topology-gg/fountain" target="_blank" rel="noopener noreferrer">Fountain</a>, a mini physics engine in Cairo</p>
+      <p>
+        {' '}
+        powered by{' '}
+        <a href="https://github.com/topology-gg/fountain" target="_blank" rel="noopener noreferrer">
+          Fountain
+        </a>
+        , a mini physics engine in Cairo
+      </p>
 
       {/* <h3>View level</h3>
       <form onSubmit={handleSubmit(onSubmitLevel)}>
@@ -87,28 +108,32 @@ const Home: NextPage = () => {
       <p>level id: 0 or 1</p>
       <p>move.x & move.y: float; check `game.cairo` for constraints</p>
       <form onSubmit={handleSubmit(onSubmitMove)}>
-        <input defaultValue="level id" {...register("levelRequired", { required: true })} />
+        <input placeholder="level id" {...register('levelRequired', { required: true })} />
         {errors.levelRequired && <span> (This field is required) </span>}
 
-        <input defaultValue="move.x" {...register("moveXRequired", { required: true })} />
+        <input placeholder="move.x" {...register('moveXRequired', { required: true })} />
         {errors.moveXRequired && <span> (This field is required) </span>}
 
-        <input defaultValue="move.y" {...register("moveYRequired", { required: true })} />
+        <input placeholder="move.y" {...register('moveYRequired', { required: true })} />
         {errors.moveYRequired && <span> (This field is required) </span>}
 
         <input type="submit" />
       </form>
 
       <h3>System status</h3>
-      <p> {'>'} Number of solutions found: { solutionCountValue?.count }</p>
-      <div> {'>'} Scoreboard: { htmlParse(html_string) }</div>
+      <p>
+        {' '}
+        {'>'} Number of solutions found: {solutionCountValue?.count}
+      </p>
+      <div>
+        {' '}
+        {'>'} Scoreboard: {htmlParse(html_string)}
+      </div>
 
       <DemoTransactionManager />
-
     </div>
   )
 }
-
 
 function DemoTransactionManager() {
   const { transactions, removeTransaction } = useStarknetTransactionManager()
@@ -119,24 +144,14 @@ function DemoTransactionManager() {
         {transactions.length === 0
           ? 'No transactions'
           : transactions.map((tx, index) => (
-              <TransactionItem
-                key={index}
-                transaction={tx}
-                onClick={() => removeTransaction(tx.transactionHash)}
-              />
+              <TransactionItem key={index} transaction={tx} onClick={() => removeTransaction(tx.transactionHash)} />
             ))}
       </div>
     </div>
   )
 }
 
-function TransactionItem({
-  transaction,
-  onClick,
-}: {
-  transaction: Transaction
-  onClick: () => void
-}) {
+function TransactionItem({ transaction, onClick }: { transaction: Transaction; onClick: () => void }) {
   return (
     <div>
       {transaction.status}: {transaction.transactionHash} <button onClick={onClick}>remove</button>
