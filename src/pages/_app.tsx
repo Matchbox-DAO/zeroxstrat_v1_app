83287c3d0dfa57c2e8adf9979acab9ce96dd8ee1
dhruvkelawala <dhruvrk2000@gmail.com>
2022-03-23 21:27:09 +0530

    add theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import NextHead from 'next/head'
+import { FixedGlobalStyle, ThemedGlobalStyle } from '../theme'
 import { StarknetProvider } from '@starknet-react/core'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -8,6 +9,8 @@ function MyApp({ Component, pageProps }: AppProps) {
       <NextHead>
         <title>0xstrat v1.0</title>
       </NextHead>
+      <FixedGlobalStyle />
+      <ThemedGlobalStyle />
       <Component {...pageProps} />
     </StarknetProvider>
   )
