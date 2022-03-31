c5dbfc9f1741bac59f7d5d03fabf4191bc77a2a2
Tim <timour.almazov@polyconseil.fr>
2022-03-31 17:06:25 +0200

    correct error in sns hook

--- src/hooks/sns.ts
+++ src/hooks/sns.ts
@@ -5,7 +5,7 @@ import SNSAbi from '~/abi/sns_abi.json'
 
 export function useSNSContract() {
   return useContract({
-    abi: SNSAbi.abi as Abi[],
+    abi: SNSAbi.abi as Abi,
     address: '0x02ef8e28b8d7fc96349c76a0607def71c678975dbd60508b9c343458c4758fac',
   })
 }
